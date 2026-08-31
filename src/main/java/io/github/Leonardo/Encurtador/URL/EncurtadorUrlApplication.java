5292df77aff3ce67b828a779db3ed0b25114172f
braga <bragaleonardo85@gmail.com>
2026-08-31 20:20:15 -0300

    feat: entidade url e migration do banco de dados

---
+++ src/main/java/io/github/Leonardo/Encurtador/URL/EncurtadorUrlApplication.java
@@ -0,0 +1,13 @@
+package io.github.Leonardo.Encurtador.URL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorUrlApplication.class, args);
+	}
+
+}
---
+++ src/main/java/io/github/Leonardo/Encurtador/URL/entities/Url.java
@@ -0,0 +1,23 @@
+package io.github.Leonardo.Encurtador.URL.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "urls")
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "short_code")
+    private String shortCode;
+
+    @Column(name = "original_url")
+    private String originalUrl;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
